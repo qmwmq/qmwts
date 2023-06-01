3c90b940d5a862fca9fbce18945016063e30e12e
qmw <123349527@qq.com>
2023-06-01 17:15:29 +0800

    init

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+function add(a) {
+    console.log(a);
+}
+exports.add = add;
---
+++ index.ts
@@ -0,0 +1,4 @@
+export function add(a: number) {
+    console.log(a)
+}
+// https://www.jianshu.com/p/8fa2c50720e4
