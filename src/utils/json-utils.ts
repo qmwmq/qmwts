e204fb8dd2d926bb7822566df86bc331df8685d6
qmw <123349527@qq.com>
2023-08-04 14:26:07 +0800

    添加uuid工具

--- src/utils/json-utils.ts
+++ src/utils/json-utils.ts
@@ -1,42 +1,42 @@
 export default {
-    isPrototypeObject(o: any): boolean {
-        return Object.prototype.toString.call(o) === '[object Object]'
-    },
-    isPrototypeArray(o: any): boolean {
-        return Object.prototype.toString.call(o) === '[object Array]'
-    },
-    isJSONObject(o: any): boolean {
-        if (this.isPrototypeObject(o))
-            return true
-        try {
-            return this.isPrototypeObject(JSON.parse(o))
-        } catch (e) {
-            return false
-        }
-    },
-    isJSONArray(o: any): boolean {
-        if (this.isPrototypeArray(o))
-            return true
-        try {
-            return this.isPrototypeArray(JSON.parse(o))
-        } catch (e) {
-            return false
-        }
-    },
-    toJSONObject<T>(o: any): T {
-        if (this.isPrototypeObject(o))
-            return <T>o
-        return this.isJSONObject(o) ? <T>JSON.parse(o) : <T>{}
-    },
-    toJSONArray<T>(o: any): T[] {
-        if (this.isPrototypeArray(o))
-            return <T[]>o
-        return this.isJSONArray(o) ? <T[]>JSON.parse(o) : <T[]>[]
-    },
-    optionalChaining(o: any = {}, chain: string): any {
-        const chaining = chain.split('.')
-        for (const key of chaining)
-            o = o[key] || ''
-        return o
+  isPrototypeObject(o: any): boolean {
+    return Object.prototype.toString.call(o) === '[object Object]'
+  },
+  isPrototypeArray(o: any): boolean {
+    return Object.prototype.toString.call(o) === '[object Array]'
+  },
+  isJSONObject(o: any): boolean {
+    if (this.isPrototypeObject(o))
+      return true
+    try {
+      return this.isPrototypeObject(JSON.parse(o))
+    } catch (e) {
+      return false
     }
+  },
+  isJSONArray(o: any): boolean {
+    if (this.isPrototypeArray(o))
+      return true
+    try {
+      return this.isPrototypeArray(JSON.parse(o))
+    } catch (e) {
+      return false
+    }
+  },
+  toJSONObject<T>(o: any): T {
+    if (this.isPrototypeObject(o))
+      return <T>o
+    return this.isJSONObject(o) ? <T>JSON.parse(o) : <T>{}
+  },
+  toJSONArray<T>(o: any): T[] {
+    if (this.isPrototypeArray(o))
+      return <T[]>o
+    return this.isJSONArray(o) ? <T[]>JSON.parse(o) : <T[]>[]
+  },
+  optionalChaining(o: any = {}, chain: string): any {
+    const chaining = chain.split('.')
+    for (const key of chaining)
+      o = o[key] || ''
+    return o
+  }
 }
--- src/utils/number-utils.ts
+++ src/utils/number-utils.ts
@@ -1,26 +1,26 @@
 export default {
-    // 判断是否数字
-    ifNaN(number: any, substitute: any): any {
-        return this.isNumber(number) ? +number : substitute
-    },
-    isNumber(number: any): boolean {
-        number = String(number).trim()
-        return number !== '' && isFinite(+number) && !isNaN(+number)
-    },
-    // 增加千分位分隔符
-    thousandths(number: any, fixed: number = 2): string {
-        if (!this.isNumber(number))
-            return ''
-        number = (+number).toFixed(fixed)
-        return number.replace(/\d+/, (x: string) => {
-            return x.replace(/(\d)(?=(\d{3})+$)/g, (y: string) => {
-                return y + ','
-            })
-        })
-    },
-    summation(array: any[] = []): number {
-        return array.reduce((prev, curr) => {
-            return prev + this.ifNaN(curr, 0)
-        }, 0)
-    }
+  // 判断是否数字
+  ifNaN(number: any, substitute: any): any {
+    return this.isNumber(number) ? +number : substitute
+  },
+  isNumber(number: any): boolean {
+    number = String(number).trim()
+    return number !== '' && isFinite(+number) && !isNaN(+number)
+  },
+  // 增加千分位分隔符
+  thousandths(number: any, fixed: number = 2): string {
+    if (!this.isNumber(number))
+      return ''
+    number = (+number).toFixed(fixed)
+    return number.replace(/\d+/, (x: string) => {
+      return x.replace(/(\d)(?=(\d{3})+$)/g, (y: string) => {
+        return y + ','
+      })
+    })
+  },
+  summation(array: any[] = []): number {
+    return array.reduce((prev, curr) => {
+      return prev + this.ifNaN(curr, 0)
+    }, 0)
+  }
 }
