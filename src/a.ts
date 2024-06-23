a826d567a4bbd1ecebb86ef3452c95a4f768a155
qmw <123349527@qq.com>
2024-06-23 18:36:16 +0800

    init

--- src/a.ts
+++
@@ -1,2 +0,0 @@
-// 获取挂载元素的样式
-// window.getComputedStyle(this.$refs.xxx)
--- src/utils/number-utils.ts
+++ src/utils/number-utils.ts
@@ -11,16 +11,15 @@ export default {
   },
   // 增加千分位分隔符
   thousandths(number: any, fixed: number = 2): string {
-    return 'aaa'
-    // if (!this.isNumber(number))
-    //   return ''
-    // number = new Intl.NumberFormat('en-US', {
-    //   minimumFractionDigits: fixed,
-    //   maximumFractionDigits: fixed,
-    // }).format(number)
-    // if (+number === 0)// -0转为普通0
-    //   return (0).toFixed(fixed)
-    // return number
+    if (!this.isNumber(number))
+      return ''
+    number = new Intl.NumberFormat('en-US', {
+      minimumFractionDigits: fixed,
+      maximumFractionDigits: fixed,
+    }).format(number)
+    if (+number === 0)// -0转为普通0
+      return (0).toFixed(fixed)
+    return number
   },
   summation(array: any[] = []): number {
     return array.reduce((prev, curr) => prev + this.ifNaN(curr, 0), 0)
