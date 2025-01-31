edb1ab0f408b16c26a709a5a285604cfafbef01f
12334 <123349527@qq.com>
2025-01-31 21:50:53 +0800

    1.优化ArrayUtils.arrayToTree方法的最上级判断方式

--- src/utils/array-utils.ts
+++ src/utils/array-utils.ts
@@ -13,18 +13,20 @@ export default {
       childrenKey: string = 'children'
   ): T[] {
     const map = new Map() // 先根据每项的parentId将该项放入Map
+    const ids = [] // 记录所有的id，parentId不在这个集合内则说明是最上级
     for (let i = array.length - 1; i >= 0; i--) {
-      const { [i]: e } = array, { [parentKey]: pid } = e
+      const { [i]: e } = array, { [idKey]: id, [parentKey]: pid } = e
       const children = map.get(pid) || []
       children.unshift({ ...e })
       map.set(pid, children)
+      ids.push(id)
     }
     const o = []
     const values = Array.from(map.values()).flat()
     for (let i = values.length - 1; i >= 0; i--) {
       const { [i]: e } = values, { [idKey]: id, [parentKey]: pid } = e
       e[childrenKey] = map.get(id) // 赋值children
-      if (!pid) // 只返回最上级
+      if (!ids.includes(pid)) // 只返回最上级
         o.unshift(e)
     }
     return o
